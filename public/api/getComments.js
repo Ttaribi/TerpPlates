f4b1154ed8d5f8501910103efc0e1a221195dd63
Opubo T <m.taribi18@gmail.com>
2024-06-02 19:08:34 -0400

    adding mongo and node

---
+++ public/api/getComments.js
@@ -0,0 +1,20 @@
+// api/getComments.js
+const MongoClient = require('mongodb').MongoClient;
+
+module.exports = async (req, res) => {
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+
+    const comments = await client.db("your-database-name").collection("comments").find().toArray();
+
+    res.status(200).json(comments);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error retrieving comments');
+  } finally {
+    await client.close();
+  }
+};
---
+++ public/api/submit.js
@@ -0,0 +1,21 @@
+// api/submit.js
+const MongoClient = require('mongodb').MongoClient;
+
+module.exports = async (req, res) => {
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+
+    const commentData = req.body;
+    const result = await client.db("your-database-name").collection("comments").insertOne(commentData);
+
+    res.status(200).send('Comment saved');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error saving comment');
+  } finally {
+    await client.close();
+  }
+};
